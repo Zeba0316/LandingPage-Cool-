743a635ac68e06fdf0e38a0a905862f674a0bee5
Raza <razasfs2003@gmail.com>
2024-06-06 19:28:00 +0530

    homeAlmostDone

--- my-first-nextapp/src/app/components/Home.js
+++ my-first-nextapp/src/app/components/Home.js
@@ -5,9 +5,38 @@ import Nav from './Nav'
 const Hero = () => {
     return (
         <div className='hero' >
-            <div className='left'></div>
-            <div className='mid'></div>
-            <div className='right'></div>
+            <div className='left'>
+                <div>
+                    <p>Our Summer Sale</p>
+                    <p>is Still Going!</p>
+                </div>
+                <div>
+                    <img src="./leftArrow.png" />
+                    <img style={{ position: "absolute", top: "15%", height: "150px", width: "150px" }} src="./blueberry.png" />
+                </div>
+            </div>
+            <div className='mid'>
+                <div>
+                    <img src='./homeCanBg.png'/>
+                </div>
+            </div>
+            <div className='right'>
+                <div>
+                    <div style={{
+                        flex: 1
+                    }}></div>
+                    <div style={{ flex: 1 }}>
+                        <p>we can our premium, organic</p>
+                        <p>beans for 16 hours in cold filtered</p>
+                        <p>water to extract all things you</p>
+                        <p>love about coffee</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="./leftArrow.png" />
+                    <img style={{ position: "absolute", top: "15%", height: "150px", width: "150px" }} src="./orange.png" />
+                </div>
+            </div>
         </div>
     )
 }
