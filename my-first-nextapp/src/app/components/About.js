8cbbc6f14a36ccfcd782a4fa3c4b382ce6610eb6
Raza <razasfs2003@gmail.com>
2024-06-07 02:04:44 +0530

    about upper done

--- my-first-nextapp/src/app/components/About.js
+++ my-first-nextapp/src/app/components/About.js
@@ -4,8 +4,34 @@ import "../css/about.scss"
 function About() {
     return (
         <div className='about'>
-            <div className='up'></div>
-            <div className="down"></div>
+            {/* upper section */}
+            <div className='up'>
+                <p>Live Your Life With Full</p>
+                <p>Of Freshness</p>
+                <div>
+                    <div>
+                        <p>Only 100</p>
+                        <p>Calories</p>
+                    </div>
+                    <div>
+                        <p>Zero</p>
+                        <p>Percentage</p>
+                        <p>Sugar</p>
+                    </div>
+                    <div>
+                    <p>Gluten Free</p>
+                    <p>Product</p>
+                    </div>
+                    <div>
+                    <p>Lightly</p>
+                    <p>Carbonated</p>
+                    </div>
+                </div>
+            </div>
+            {/* lower section */}
+            <div className="down">
+
+            </div>
         </div>
     )
 }
