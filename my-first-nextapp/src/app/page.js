b8869b09330dbc510315ef0e48135db21bcc0222
zeba <zebakhan0316@gmail.com>
2024-06-07 21:05:21 +0530

    gallery done

--- my-first-nextapp/src/app/page.js
+++ my-first-nextapp/src/app/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import About from './components/About'
+import Gallery from './components/Gallery'
 
 function page() {
   return (
     <div style={{ flex: 1, }}>
       <Home />
       <About />
+      <Gallery/>
       <Footer />
     </div>
   )
