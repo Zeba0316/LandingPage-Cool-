dfdbed25eeb65a6f31259628e73db35e6647c2c1
Raza <razasfs2003@gmail.com>
2024-06-08 17:53:10 +0530

    product component responsiveness done !

--- my-first-nextapp/src/app/components/Footer.js
+++ my-first-nextapp/src/app/components/Footer.js
@@ -18,44 +18,44 @@ function Footer() {
           </div>
         </div>
         <div className="midFoot">
-            <div className="midFoot-row">
-                <h2>Menu</h2>
-                <ul>
-                    <li><a href="#">About us</a></li>
-                    <li><a href="#">Recent Products</a></li>
-                    <li><a href="#">Featured Products</a></li>
-                    <li><a href="#">Testimonials</a></li>
-                    <li><a href="#">Products's Gallery</a></li>
-                </ul>
-            </div>
-            <div className="midFoot-row">
-                <h2>Company</h2>
-                <ul>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Terms & Conditions</a></li>
-                    <li><a href="#">Privacy & Policies</a></li>
-                    <li><a href="#">Shipping Methods</a></li>
-                    <li><a href="#">Support</a></li>
-                </ul>
-            </div>
+          <div className="midFoot-row">
+            <h2>Menu</h2>
+            <ul>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Recent Products</a></li>
+              <li><a href="#">Featured Products</a></li>
+              <li><a href="#">Testimonials</a></li>
+              <li><a href="#">Products's Gallery</a></li>
+            </ul>
+          </div>
+          <div className="midFoot-row">
+            <h2>Company</h2>
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">Terms & Conditions</a></li>
+              <li><a href="#">Privacy & Policies</a></li>
+              <li><a href="#">Shipping Methods</a></li>
+              <li><a href="#">Support</a></li>
+            </ul>
+          </div>
         </div>
         <div className="leftFoot">
-            <h2>Newsletter</h2>
-            <input  style={{color:"black"}} placeholder="Enter your email" type="email"/>
-            <button>Subscribe</button>
+          <h2>Newsletter</h2>
+          <input style={{ color: "black" }} placeholder="Enter your email" type="email" />
+          <button>Subscribe</button>
         </div>
       </div>
       <div className="footsec2">
         <div className="socials">
-            <p style={{color:"lightgray"}} >Facebook</p>
-            <p style={{color:"lightgray"}}>Twitter</p>
-            <p style={{color:"lightgray"}}>Instagram</p>
+          <p style={{ color: "lightgray" }}>Facebook</p>
+          <p style={{ color: "lightgray" }}>Twitter</p>
+          <p style={{ color: "lightgray" }}>Instagram</p>
         </div>
         <div className="rights">
-            <p style={{color:"lightgray"}}>All rights reserved to <span style={{color:"white"}}>CreativeALiens</span></p>
+          <p style={{ color: "lightgray" }}>All rights reserved to <span style={{ color: "white" }}>CreativeALiens</span></p>
         </div>
         <div className="info">
-            <p style={{color:"lightgray"}}>info@creativealiens.com</p>
+          <p style={{ color: "lightgray" }}>info@creativealiens.com</p>
         </div>
       </div>
     </div>
